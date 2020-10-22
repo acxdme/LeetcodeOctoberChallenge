280895eb0975c5052ad3512fb7594ec88818368b
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-10-22 11:45:28 +0530

    Update Day-21(AsteroidCollision).cpp

--- Day-21(AsteroidCollision).cpp
+++ Day-21(AsteroidCollision).cpp
@@ -1,5 +1,60 @@
 /*
-Time complexity :
-Space complexity :
-My approach :
+Time complexity : O(N)
+Space complexity : O(N) , for the stack
+My approach : negative asteroid brings disturbance , stablize rest of the elements present in the stack.
 */
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) 
+    {
+        int n =asteroids.size();
+        stack<int> s;
+        for(int i=0;i<asteroids.size();i++)
+        {
+            if(s.empty())
+            {
+                s.push(asteroids[i]);
+            }
+            else
+            {
+                if(asteroids[i]>0)
+                    s.push(asteroids[i]);
+                else
+                {
+                    bool flag=false;
+                    
+                    while(!s.empty() and s.top()>0 )
+                    {
+                        if(abs(asteroids[i])< s.top())
+                        {    
+                            flag=true;
+                            break;
+                        }
+                        else if(abs(asteroids[i]) > s.top())
+                            s.pop();
+                        else
+                        {
+                            flag=true;
+                            s.pop();
+                             break;
+                        }   
+                    }
+                  
+                  if(flag == false)
+                      s.push(asteroids[i]);
+                }
+            }
+        }
+        vector<int> answer;
+        while(!s.empty())
+        {
+            answer.push_back(s.top());
+            s.pop();
+        }
+        
+        reverse(answer.begin(),answer.end());
+        return answer;
+        
+    }
+};
